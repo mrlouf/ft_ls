98cd56b69a2db09f349764f53984627efb024385
Nicolas <ponchon.nicolas@gmail.com>
2025-09-16 14:29:12 +0200

    Modified some test folders to include spaces for testing purposes

---
+++ srcs/print.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nicolas <nicolas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/13 12:02:14 by nponchon          #+#    #+#             */
+/*   Updated: 2025/09/16 13:37:07 by nicolas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incs/ft_ls.h"
+
+void	display_help(t_ls *ls)
+{
+	ft_printf("Usage: ./ft_ls [OPTION]... [FILE]...\n\
+List information about the FILEs (the current directory by default).\n\
+\n\
+Available options:\n\
+	-a		do not ignore entries starting with .\n\
+	-l		use a long listing format\n\
+	-r		reverse order while sorting\n\
+	-R		list subdirectories recursively\n\
+	-t		sort by time, newest first\n\
+\n\
+	--help	display this help and exit\n");
+	ls_exit(ls, 0, NULL);
+}
+
+void    ls_print_dir(DIR *dir)
+{
+    struct dirent *entry;
+    entry = readdir(dir);
+    while (entry)
+    {
+        if (ft_strncmp(entry->d_name, ".", 1) == 0)
+        {
+            entry = readdir(dir);
+            continue ;
+        }
+        ft_printf("%s  ", entry->d_name);
+        entry = readdir(dir);
+    }
+}
+
+void    ls_print_final(t_ls *ls)
+{
+    int i = 0;
+    while (i < ls->file_size)
+    {
+        if (ls->file_size > 1)
+            ft_printf("%s:\n", ls->dir_entries[i].dirname);
+        t_list *tmp = ls->dir_entries[i].entries;
+        while (tmp)
+        {
+            ft_printf("%s  ", (char *)tmp->content);
+            tmp = tmp->next;
+        }
+        if (ls->file_size > 1 && i < ls->file_size - 1)
+            ft_printf("\n\n");
+        i++;
+    }
+    ft_printf("\n");
+    return ;
+}
---
+++ srcs/sort.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nicolas <nicolas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/16 13:23:28 by nicolas           #+#    #+#             */
+/*   Updated: 2025/09/16 13:27:09 by nicolas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incs/ft_ls.h"
+
+void	ls_sort_entries(t_ls *ls)
+{
+    int i = 0;
+
+    while (i < ls->file_size)
+    {
+        if (ls->flag_reverse) {
+            ft_list_reverse(&ls->dir_entries[i].entries, &ft_lst_cmp);
+        } else { 
+            ft_list_sort(&ls->dir_entries[i].entries, &ft_lst_cmp);
+        }
+        i++;
+    }
+    return ;
+}
